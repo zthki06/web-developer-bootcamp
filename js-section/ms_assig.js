d10f23ea6be962f4fc8a72773ae6dcacfc0181fb
zthki06 <zthki06@gmail.com>
2024-04-29 10:38:47 +0200

    js strings section assign

---
+++ js-section/ms_assig.js
@@ -0,0 +1,6 @@
+// DON'T TOUCH THIS LINE! (please)
+const word = "skateboard"; //Don't change this line!
+
+// YOUR CODE BELOW THIS LINE:
+let facialHair = word.slice(5);
+facialHair = facialHair.replace("o", "e");
