fa54a72e9ce4bac8714a853bc46ea4ab23ba3a57
zthki06 <zthki06@gmail.com>
2024-04-30 16:47:26 +0200

    Implement add - list - delete logic

--- js-section/todoList/src.js
+++ js-section/todoList/src.js
@@ -1,1 +1,24 @@
-console.log("hello");
+let lst = [];
+
+console.log("Welcome to our todo list");
+let cmd = prompt("Do you want to continue or quit");
+
+while (cmd !== "quit") {
+  cmd = prompt("Choose one option: new - list - delete - quit");
+  if (cmd === "new") {
+    let article = prompt("Add a todo");
+    lst.push(article);
+  } else if (cmd === "list") {
+    for (let l of lst) {
+      console.log(l);
+    }
+  } else if (cmd === "delete") {
+    console.log("Your current list");
+    for (let i = 0; i < lst.length; i++) {
+      console.log(`${i} - ${lst[i]}`);
+    }
+    let element = parseInt(prompt("Choose the element you want to delete"));
+    lst.splice(element, 1);
+  }
+}
+console.log("You quit the app!");
