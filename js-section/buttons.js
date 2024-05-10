e45548df23d853bc7e5f39bb5a855c3aa9a820d5
zthki06 <zthki06@gmail.com>
2024-05-10 17:05:13 +0200

    Update section dom assign

---
+++ js-section/buttons.js
@@ -0,0 +1,7 @@
+// WRITE YOUR CODE IN HERE:
+for (let i = 0; i < 100; i++) {
+  let btn = document.createElement("button");
+  btn.innerText = "Hey!";
+  let div = document.querySelector("div");
+  div.appendChild(btn);
+}
